19ed28235d5a849bc3f305844bf81b26a03b5cb4
takimi12 <tomek12olech@gmail.com>
2023-11-08 00:17:23 +0100

    Product Card

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { products } from "@/utils/products";
 import HomeBanner from "./components/HomeBanner";
 import Container from "./components/container";
-import { truncateText } from "@/utils/truncateText";
+import ProductCard from "./components/products/ProductCard";
 
 export default function Home() {
   return (
@@ -9,12 +9,19 @@ export default function Home() {
       <Container>
         <div>
           <HomeBanner />
+          </div>
+          <div className="grid grid-cols-2
+           sm:grid-cols-3
+           lg:grid-cols-4
+           xl:grid-cols-5
+           2xl:grid-cols-6 
+           gap:8
+           ">
         {products.map((product: any) => {
-          return <div key={product.id}>{truncateText(product.name)}</div>;
+          return <ProductCard  data={product}/>;
         })}
-        
         </div>
       </Container>
     </div>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
 
-module.exports = nextConfig
+    images: {
+      domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
