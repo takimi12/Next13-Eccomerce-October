58e7cef2ac96576a4472a31eecd10829821e9927
takimi12 <tomek12olech@gmail.com>
2023-11-16 15:22:29 +0100

    Manage Products

--- app/admin/manage-products/page.tsx
+++ app/admin/manage-products/page.tsx
@@ -1,7 +1,23 @@
-const ManageProducts = () => {
+import { Container } from "@mui/material";
+import ManageProductsClient from "./ManageProductsClient";
+import getProducts from "@/actions/getProducts";
+import getCurrentUser from "@/actions/getCurrentUser";
+import NullData from "@/app/components/NullData";
+
+const ManageProducts = async () => {
+
+    const products = await getProducts({category: null})
+    const currentUser = await getCurrentUser()
+
+    if(!currentUser || currentUser.role !== 'ADMIN') {
+        return <NullData title="Ooops! Acces denied" />;
+    }
+
     return ( 
-        <div>
-            <h1>Manage Products</h1>
+        <div className="pt-8">
+            <Container >
+            <ManageProductsClient products={products} />
+            </Container>
         </div>
      );
 }
