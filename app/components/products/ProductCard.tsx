3eb9374f171ef0a8f2a2fb995062fd6c27ecd44e
takimi12 <tomek12olech@gmail.com>
2023-11-18 03:14:10 +0100

    Problem solving

--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -1,37 +1,40 @@
 "use client";
 
-import { truncateText } from "@/utils/truncateText";
-import { truncate } from "fs";
-import Image from "next/image";
 import { formatPrice } from "@/utils/formatPrice";
+import { truncateText } from "@/utils/truncateText";
 import { Rating } from "@mui/material";
+import { Product, Review } from "@prisma/client";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
-  data: any
+  data: Product & {
+    reviews: Review[];
+  };
+  shuffledProducts: Product[];
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({
-data }) => {
+  data,
+  shuffledProducts,
+}) => {
+  console.log("shuffledProducts", shuffledProducts);
+  const router = useRouter();
 
-    const router = useRouter();
-
-    const productRating = 
-    data.reviews.reduce((acc:number, item:any) => 
-    item.rating + acc, 0) /
-     data.reviews.length;
-
+  const ProductRating =
+    data.reviews.reduce((acc, item) => item.rating + acc, 0) /
+    data.reviews.length;
 
   return (
     <div
-        onClick={() => router.push(`/product/${data.id}`)}
+      onClick={() => router.push(`/product/${data.id}`)}
       className="
         col-span-1
         cursor-pointer
         border-[1.2px]
         border-slate-200
         bg-slate-50
-        rounded-sm
+        rounded-md
         p-2
         transition
         hover:scale-105
@@ -39,12 +42,7 @@ data }) => {
         text-sm
       "
     >
-      <div className="
-      flex 
-      flex-col 
-      items-center
-       w-full 
-       gap-1">
+      <div className="flex flex-col items-center w-full gap-1">
         <div
           className="
             aspect-square
@@ -54,23 +52,19 @@ data }) => {
             "
         >
           <Image
-            src={data.images[0].image}
-            alt={data.name}
             fill
             className="
             w-full
             h-full
             object-contain
             "
+            src={data.images[0].image}
+            alt={data.name}
           />
         </div>
-        <div className="mt-4">
-            {truncateText(data.name)}
-        </div>
-        <div>
-            <Rating value={productRating} readOnly/>
-        </div>
-        <div>{data.reviews.lenght} reviews</div>
+        <div className="mt-4">{truncateText(data.name)}</div>
+        <Rating value={ProductRating} readOnly />
+        <div>{data.reviews.length} reviews</div>
         <div className="font-semibold">{formatPrice(data.price)}</div>
       </div>
     </div>
